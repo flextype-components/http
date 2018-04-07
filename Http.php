fc5ab358270ace38161739c76c0c297dbb199930
Awilum <awilum@msn.com>
2018-04-07 03:38:55 +0300

    header added

--- Http.php
+++ Http.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * @package Flextype Components
+ *
+ * @author Sergey Romanenko <awilum@yandex.ru>
+ * @link http://components.flextype.org
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Flextype\Component\Http;
 
 use Flextype\Component\Arr\Arr;
